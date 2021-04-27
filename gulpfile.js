2e920717470dc59628aef2112d784d2bfef2a637
gmvqbu <gmvqbu@gmail.com>
2021-04-27 11:08:07 +0200

    setup

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp');
+const sassdoc = require('sassdoc');
+
+const paths = {
+  sassdoc: {
+    src: './src/**/*.scss',
+    dest: './docs',
+    description: './README.md'
+  }
+};
+
+gulp.task('sassdoc', function () {
+  const options = {
+    dest: paths.sassdoc.dest,
+    exclude: [
+      './src/helpers/_errors.scss'
+    ],
+    theme: 'default',
+    package: './package.json',
+    autofill: ['content'],
+    groups: {
+      'error': 'Error',
+      'helpers-list': 'Helpers — List',
+      'helpers-map': 'Helpers — Map',
+      'helpers-string': 'Helpers — String',
+      'modules-theme': 'Modules — Theme'
+    },
+    'no-update-notifier': false,
+    verbose: false,
+    strict: false,
+    verbose: true,
+    display: {
+      access: ['public'],
+      alias: true,
+      watermark: true,
+    },
+    basePath: 'https://github.com/gmvqbu/duo',
+    // shortcutIcon: '',
+    sort: [
+      'access',
+      'group',
+      'line',
+    ],
+    descriptionPath: paths.sassdoc.description
+  };
+
+  return gulp.src(paths.sassdoc.src)
+    .pipe(sassdoc(options));
+});
